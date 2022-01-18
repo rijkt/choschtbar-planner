ba7e5c9ce34cefdd4ba25d3a04c3a83b949faecb
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2022-01-18 19:32:29 +0100

    read shifts from backend

--- src/choschtbar_planner/core.cljs
+++ src/choschtbar_planner/core.cljs
@@ -1,16 +1,27 @@
 (ns choschtbar-planner.core
+  (:require-macros [cljs.core.async.macros :refer [go]]) ; for cljs-http
   (:require [reagent.core :as reagent :refer [atom]]
             ["react-big-calendar" :refer (Calendar momentLocalizer)]
-            ["moment" :as moment]))
+            ["moment" :as moment]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:localizer (momentLocalizer moment)}))
+(defonce app-state (atom {:shifts [] :localizer (momentLocalizer moment)}))
+
+(go (let [response (<! (http/post "https://hybndamir4.execute-api.eu-central-1.amazonaws.com/default/getShifts"
+                                  {:with-credentials? false}))]
+      (swap! app-state assoc :shifts (:body response))))
+
+(defn to-event [shift]
+  {:title (str (:location shift) " - " (:notes shift))
+   :start (moment/unix (:startTime shift)) :end (moment/unix (:endTime shift))})
 
 (defn main []
   [:div
    [:h1.text-4xl.mt-2.font-normal "Deine Touren"]
-   (let [events  [{:title "Test" :start (js/Date.) :end (.add (moment) 2 "hours")}]]
+   (let [events (map to-event (:shifts @app-state))]
      [(reagent/adapt-react-class Calendar) {:localizer (:localizer @app-state) :events events
                                             :style {:height 500} :culture "de-CH"}])])
 

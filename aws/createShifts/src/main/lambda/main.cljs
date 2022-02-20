372cbbc562f6fa07afb443d1449e6d858a631425
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2022-02-20 11:25:52 +0100

    extract getting the db to fn

--- aws/createShifts/src/main/lambda/main.cljs
+++ aws/createShifts/src/main/lambda/main.cljs
@@ -2,7 +2,7 @@
   (:require
    ["@aws-sdk/client-s3" :refer (S3Client GetObjectCommand)]
    ["node-fetch" :refer (Response)]
-   [cljs.core.async :refer [go <! >! chan]]
+   [cljs.core.async :refer [go <! >! chan put!]]
    [cljs.core.async.interop :refer-macros [<p!]]))
 
 (def ^js client (S3Client. (clj->js {:region "eu-central-1"})))
@@ -17,18 +17,24 @@
   (new js/Promise (fn [resolve _] 
                     (go (resolve (<! channel))))))
 
-(defn handler [event context]
-  (let [chanl (chan 1)
-        promise (wrap-as-promise chanl)
-        cmd-input (clj->js {:Bucket "choschtbar-data" :Key "db.json"})
+(defn get-db [channel]
+  (let [cmd-input (clj->js {:Bucket "choschtbar-data" :Key "db.json"})
         cmd (GetObjectCommand. cmd-input)]
     (go
-      (-> (.send client cmd)
-          (<p!)
-          (.-Body) ; IncomingMessage : stream.Readable
-          (Response.)
-          (.json)
-          (<p!)
-          (prn))
-      (>! chanl dummy))
-    promise)) ; make it an async function handler to avoid callback hell
+      (put! channel (-> (.send client cmd)
+                        (<p!)
+                        (.-Body) ; IncomingMessage : stream.Readable
+                        (Response.)
+                        (.json)
+                        (<p!)
+                        (js->clj :keywordize-keys true))))))
+
+(defn handler [event context]
+  (let [response-c (chan 1)
+        response-promise (wrap-as-promise response-c)
+        db-c (chan 1)]
+    (get-db db-c)
+    (go
+      (prn (<! db-c))
+      (>! response-c dummy))
+    response-promise)) ; make it an async function handler to avoid callback hell

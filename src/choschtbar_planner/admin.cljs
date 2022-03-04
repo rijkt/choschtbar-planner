e93860566c8bc8e05320c3469981c824fa6651b0
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2022-03-04 12:14:57 +0100

    add api call

--- src/choschtbar_planner/admin.cljs
+++ src/choschtbar_planner/admin.cljs
@@ -1,13 +1,33 @@
 (ns choschtbar-planner.admin
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [cljs.core.async :refer [go chan <!]]
+            [cljs-http.client :as http]))
 
 (defn- assoc-for [key state]
   (fn [e] (swap! state assoc key (-> e .-target .-value))))
 
+(defn- submit [s]
+  (fn [e]
+    (.preventDefault e)
+                                        ; form-wide validation via e.target goes here
+    (let [date (:date @s)
+          body {:month (.substring date 0 7)
+                :startTime 1645981200 ; todo 
+                :endTime 1645984800 ; todo
+                :color (:color @s)
+                :volunteer nil
+                :location (:tour @s)
+                :notes (:notes @s)}
+          response-chan (chan 1 (map :status))]
+      (go
+        (-> "https://hybndamir4.execute-api.eu-central-1.amazonaws.com/default/create-shift"
+            (http/post {:json-params body :with-credentials? false :channel response-chan}))
+        (swap! s assoc :response (<! response-chan))))))
+
 (defn root []
-  (let [s (atom {})]
+  (let [s (atom {:response ""})]
     (fn []
-      [:form.grid.grid-cols-2.gap-2 {:on-submit (fn [e] (.preventDefault e))}
+      [:form.grid.grid-cols-2.gap-2 {:on-submit (submit s)}
        [:label {:for :tour} "Tour"]
        [:input.border-green-800.border-2.border-solid.rounded-md
         {:type :text :name :tour :required true :on-change (assoc-for :tour s)}]
@@ -25,4 +45,5 @@
         [:option "Maxine Muster"]]
        [:label {:for :color} "Farbe"] ; todo: preselected values
        [:input {:type :color :name :color :required true :on-change (assoc-for :color s)}]
-       [:input {:type :submit :name :create :value "Schicht eintragen"}]])))
+       [:input {:type :submit :name :create :value "Schicht eintragen"}]
+       [:p (str (:response @s))]])))

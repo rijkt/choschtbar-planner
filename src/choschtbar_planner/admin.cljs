70f9f36991a301e11f6af62b7da8b1b8e3b9abf3
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2022-03-04 14:36:23 +0100

    correctly parse time from inputs

--- src/choschtbar_planner/admin.cljs
+++ src/choschtbar_planner/admin.cljs
@@ -1,19 +1,23 @@
 (ns choschtbar-planner.admin
   (:require [reagent.core :as reagent :refer [atom]]
             [cljs.core.async :refer [go chan <!]]
-            [cljs-http.client :as http]))
+            [cljs-http.client :as http]
+            ["moment" :as moment]))
 
 (defn- assoc-for [key state]
   (fn [e] (swap! state assoc key (-> e .-target .-value))))
+
+(defn- to-unix [date time]
+  (.unix (moment (str date "T" time))))
 
 (defn- submit [s]
   (fn [e]
     (.preventDefault e)
                                         ; form-wide validation via e.target goes here
     (let [date (:date @s)
-          body {:month (.substring date 0 7)
-                :startTime 1645981200 ; todo 
-                :endTime 1645984800 ; todo
+          body {:month (.substring date 0 7) ; YYYY-MM
+                :startTime (to-unix date (:start-time @s))
+                :endTime (to-unix date (:end-time @s)) 
                 :color (:color @s)
                 :volunteer nil
                 :location (:tour @s)
@@ -24,26 +28,27 @@
             (http/post {:json-params body :with-credentials? false :channel response-chan}))
         (swap! s assoc :response (<! response-chan))))))
 
+(defonce s (atom {:response ""}))
+
 (defn root []
-  (let [s (atom {:response ""})]
-    (fn []
-      [:form.grid.grid-cols-2.gap-2 {:on-submit (submit s)}
-       [:label {:for :tour} "Tour"]
-       [:input.border-green-800.border-2.border-solid.rounded-md
-        {:type :text :name :tour :required true :on-change (assoc-for :tour s)}]
-       [:label {:for :notes} "Kommentar"]
-       [:input.border-green-800.border-2.border-solid.rounded-md
-        {:type :text :name :notes :required true :on-change (assoc-for :notes s)}]
-       [:label {:for :date} "Datum"]
-       [:input {:type :date :name :date :required true :on-change (assoc-for :date s)}]
-       [:label {:for :start-time} "Von"]
-       [:input {:type :time :name :start-time :required true :on-change (assoc-for :start-time s)}]
-       [:label {:for :end-time} "Bis"]
-       [:input {:type :time :name :end-time :required true :on-change (assoc-for :end-time s)}]
-       [:label {:for :volunteer} "Freiwillige*r"]
-       [:select {:name :volunteer :required true}
-        [:option "Maxine Muster"]]
-       [:label {:for :color} "Farbe"] ; todo: preselected values
-       [:input {:type :color :name :color :required true :on-change (assoc-for :color s)}]
-       [:input {:type :submit :name :create :value "Schicht eintragen"}]
-       [:p (str (:response @s))]])))
+  (fn []
+    [:form.grid.grid-cols-2.gap-2 {:on-submit (submit s)}
+     [:label {:for :tour} "Tour"]
+     [:input.border-green-800.border-2.border-solid.rounded-md
+      {:type :text :name :tour :required true :on-change (assoc-for :tour s)}]
+     [:label {:for :notes} "Kommentar"]
+     [:input.border-green-800.border-2.border-solid.rounded-md
+      {:type :text :name :notes :required true :on-change (assoc-for :notes s)}]
+     [:label {:for :date} "Datum"]
+     [:input {:type :date :name :date :required true :on-change (assoc-for :date s)}]
+     [:label {:for :start-time} "Von"]
+     [:input {:type :time :name :start-time :required true :on-change (assoc-for :start-time s)}]
+     [:label {:for :end-time} "Bis"]
+     [:input {:type :time :name :end-time :required true :on-change (assoc-for :end-time s)}]
+     [:label {:for :volunteer} "Freiwillige*r"]
+     [:select {:name :volunteer :required true}
+      [:option "Maxine Muster"]]
+     [:label {:for :color} "Farbe"] ; todo: preselected values
+     [:input {:type :color :name :color :required true :on-change (assoc-for :color s)}]
+     [:input {:type :submit :name :create :value "Schicht eintragen"}]
+     [:p (str (:response @s))]]))

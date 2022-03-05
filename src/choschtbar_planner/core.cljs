0028e85f20ac65fafa897c910ec9180502189f5e
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2022-03-05 16:17:45 +0100

    add call to token endpoint

--- src/choschtbar_planner/core.cljs
+++ src/choschtbar_planner/core.cljs
@@ -5,6 +5,9 @@
             ["react-big-calendar" :refer (momentLocalizer)]
             [accountant.core :as accountant]
             [clojure.core.match :refer (match)]
+            [cemerick.url :refer (url url-encode)]
+            [cljs.core.async :refer [go chan <!]]
+            [cljs-http.client :as http]
             [choschtbar-planner.calendar]
             [choschtbar-planner.shift-detail]
             [choschtbar-planner.admin]))
@@ -22,19 +25,40 @@
      [:li.text-sm.font-sans.font-semibold.hover:text-green-500.mx-5.cursor-pointer
       {:onClick #(accountant/navigate! "admin")} "Administration"]
      [:li.text-sm.font-sans.font-semibold.hover:text-green-500.mx-5.cursor-pointer
-      "Logout"]]
+      "Logout"]
+     [:a {:href "https://choschtbar-planner.auth.eu-central-1.amazoncognito.com/oauth2/authorize?client_id=381pjf00caq7m17j2js95ko00b&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A8700%2F"} "Login"]]
     [:hr]]
    [:div.mt-6
-   (match [(:path @app-state)]
-          [(:or nil "/")] [choschtbar-planner.calendar/cal (:shifts @app-state) #(swap! app-state assoc :shifts %)
-                           (:localizer @app-state) #(swap! app-state assoc :selected %)]
-          ["detail"] (choschtbar-planner.shift-detail/detail (get (:shifts @app-state) (:id (:selected @app-state))))
-          ["admin"] [choschtbar-planner.admin/root]
-          :else [:p "404"])]])
+    (match [(:path @app-state)]
+           [(:or nil "/")] [choschtbar-planner.calendar/cal (:shifts @app-state) #(swap! app-state assoc :shifts %)
+                            (:localizer @app-state) #(swap! app-state assoc :selected %)]
+           ["detail"] (choschtbar-planner.shift-detail/detail (get (:shifts @app-state) (:id (:selected @app-state))))
+           ["admin"] [choschtbar-planner.admin/root]
+           :else [:p "404"])]])
+
+(defn get-token!
+  "Call token endpoint if code url param is present"
+  []
+  (when-let [code (-> js/window
+                      .-location
+                      .-href
+                      (url)
+                      :query
+                      (get "code"))]
+    (go
+      (let [token-url "https://choschtbar-planner.auth.eu-central-1.amazoncognito.com/oauth2/token"
+            form-params  {:grant_type "authorization_code"
+                          :code code
+                          :client_id "381pjf00caq7m17j2js95ko00b"
+                          :redirect_uri "http://localhost:8700/"}
+            response-chan (chan 1 (map :body))]
+        (http/post token-url {:form-params form-params :with-credentials? false :channel response-chan})
+        (swap! app-state assoc :auth (<! response-chan))))))
 
 (defn start []
   (moment/locale "de-CH") ; set up locale configuration
   (swap! app-state assoc :localizer (momentLocalizer moment)) ; save configured localizer
+  (get-token!)
   (accountant/configure-navigation!
    {:nav-handler (fn [path] (swap! app-state assoc :path path))
     :path-exists? (fn [path] true)}) ; todo: integrate bidi

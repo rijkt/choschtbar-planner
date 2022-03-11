6645da2a80d26943d4e382c94be839f121b4c76e
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2022-03-11 13:03:27 +0100

    extract variables

--- src/choschtbar_planner/auth.cljs
+++ src/choschtbar_planner/auth.cljs
@@ -5,6 +5,12 @@
 
 (defonce auth-state (atom {}))
 
+(def authorization-url "https://choschtbar-planner.auth.eu-central-1.amazoncognito.com")
+
+(def redirect-uri "http://localhost:8700/")
+
+(def client-id "381pjf00caq7m17j2js95ko00b")
+
 (defn- read-code! []
   (-> js/window
       .-location
@@ -17,23 +23,26 @@
   (when (and (nil? (:auth @auth-state))
              (nil? (read-code!)))
     (set! (.. js/window -location -href)
-          "https://choschtbar-planner.auth.eu-central-1.amazoncognito.com/oauth2/authorize?client_id=381pjf00caq7m17j2js95ko00b&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A8700%2F")))
+          (str authorization-url "/oauth2/authorize?"
+               "client_id=" client-id
+               "&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&"
+               "redirect_uri=" redirect-uri))))
 
 (defn logout! []
   (swap! auth-state assoc :auth nil)
   (set! (.. js/window -location -href)
-        "https://choschtbar-planner.auth.eu-central-1.amazoncognito.com/logout?client_id=381pjf00caq7m17j2js95ko00b&&logout_uri=http%3A%2F%2Flocalhost%3A8700%2F"))
+        (str authorization-url "/logout?client_id=" client-id "&logout_uri=" redirect-uri)))
 
 (defn get-token!
   "Call token endpoint if code url param is present"
   []
   (when-let [code (read-code!)]
     (go
-      (let [token-url "https://choschtbar-planner.auth.eu-central-1.amazoncognito.com/oauth2/token"
+      (let [token-url (str authorization-url "/oauth2/token")
             form-params  {:grant_type "authorization_code"
                           :code code
-                          :client_id "381pjf00caq7m17j2js95ko00b"
-                          :redirect_uri "http://localhost:8700/"}
+                          :client_id client-id
+                          :redirect_uri redirect-uri}
             response-chan (chan 1 (map :body))]
         (http/post token-url {:form-params form-params :with-credentials? false :channel response-chan})
         (swap! auth-state assoc :auth (<! response-chan)))))) ; todo: flatten?

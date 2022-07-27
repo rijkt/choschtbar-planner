f54959c5bb4d15223f08e939a337eb055ef96882
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2022-07-27 15:57:41 +0200

    handle month query param in lambda

--- aws/getShifts/index.js
+++ aws/getShifts/index.js
@@ -1,13 +1,13 @@
 import {S3Client, SelectObjectContentCommand} from "@aws-sdk/client-s3"; // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/index.html
 import {toUtf8} from "@aws-sdk/util-utf8-node";
 
-export const handler = async () => {
+export const handler = async (event, context) => {
     const client = new S3Client({ region: "eu-central-1" });
     const input = { // SelectObjectContentCommandInput
             Bucket: 'choschtbar-data',
             Key: 'db.json',
             ExpressionType: 'SQL',
-            Expression: 'SELECT shifts FROM S3Object[*].shifts',
+            Expression: 'SELECT shifts FROM S3Object[*].shifts', // since this is one object we can't filter here
             InputSerialization: {
                 JSON: {
                     Type: 'DOCUMENT'
@@ -31,7 +31,9 @@ export const handler = async () => {
     }
     const parsed =  JSON.parse(responseBuffer);
     const shifts = Object.values(parsed.shifts); // { [id]: shift }
+    const month = event.queryStringParameters?.month;
     const body = shifts.filter(item => item) // filter out null values and flatten
+	  .filter(item => month ? item.month === month : item);
     const response = {
         statusCode: 200,
         headers: {
